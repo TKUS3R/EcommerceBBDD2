03fcc1e49e301521a84550e3fd1e5d2412339569
TKUS3R <104871413+TKUS3R@users.noreply.github.com>
2025-02-16 22:07:36 -0300

    Update products.routes.js

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const productController = require("../controllers/product.controller");
 
 router.post("/productos", productController.createProduct);
-router.get("/", productController.getAllProducts);
+router.get("/", productController.getAllProducts);;
+router.get("/:id", productController.getProductById);
+router.put("/:id", productController.updateProduct);
+router.delete("/:id", productController.deleteProduct);
 
 module.exports = router;
