dc3185b9365b9d3cdc9c58d8405807d7f30c3f62
Valentín Campestri <dev.valentincampestri@hotmail.com>
2025-02-17 23:57:57 -0300

    .

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { createProduct, getAllProducts } = require("../controllers/products.controller");
-const { getHardcodedProducts } = require("../controllers/productController");
 
 const router = express.Router();
 const productController = require("../controllers/products.controller");
@@ -8,7 +7,6 @@ const productController = require("../controllers/products.controller");
 router.post("/productos", productController.createProduct);
 router.get("/", productController.getAllProducts);;
 router.get("/:id", productController.getProductById);
-router.get("/hardcoded", getHardcodedProducts);
 router.put("/:id", productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
 
